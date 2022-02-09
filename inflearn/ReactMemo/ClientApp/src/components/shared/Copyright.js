ecc023a5914a3478c9eaa425b8cf291352bac0b2
kimilm <gnlfla7416@gmail.com>
2022-02-09 18:48:58 +0900

    feat: site/tech list subcomponent add

--- inflearn/ReactMemo/ClientApp/src/components/shared/Copyright.js
+++ inflearn/ReactMemo/ClientApp/src/components/shared/Copyright.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 export class Copyright extends Component {
     render() {
+
+        const date = new Date();
+        const year = date.getFullYear();
+
         return(
             <div className="text-center">
-                Copyright &copy; 2022 <em>ReactMemo</em> all right reserved.
+                {/* 연산식도 가능 {year + 5} */}
+                Copyright &copy; {year} <em>ReactMemo</em> all right reserved.
             </div>
         );
     }
---
+++ inflearn/ReactMemo/ClientApp/src/components/shared/tech_list/TechList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class TechList extends Component {
+    render() {
+        return(
+            <ul>
+                <li>Azure</li>
+                <li>Bootstrap</li>
+                <li>C#</li>
+                <li>Dotnet React</li>
+            </ul>
+        );
+    }
+}
