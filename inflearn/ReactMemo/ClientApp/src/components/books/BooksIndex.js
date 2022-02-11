bee40059cc8b00fd18a07682a694e786070ca1e9
kimilm <gnlfla7416@gmail.com>
2022-02-12 06:22:23 +0900

    feat: create edit delete 버튼 생성 및 이벤트 처리

--- inflearn/ReactMemo/ClientApp/src/components/books/BooksIndex.js
+++ inflearn/ReactMemo/ClientApp/src/components/books/BooksIndex.js
@@ -9,6 +9,10 @@ export class BooksIndex extends Component {
             books: [],
             loading: true
         }
+
+        this.renderBooksTable = this.renderBooksTable.bind(this);
+        this.editBy = this.editBy.bind(this);
+        this.deleteBy = this.deleteBy.bind(this);
     }
 
     componentDidMount() {
@@ -17,15 +21,16 @@ export class BooksIndex extends Component {
         this.populateBooksDataWithAxiosAsync();
     }
 
-    static renderBooksTable(books) {
+    renderBooksTable(books) {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
                     <tr>
-                        <th>ID</th>
+                        <th>Id</th>
                         <th>Title</th>
                         <th>Description</th>
                         <th>Created</th>
+                        <th>Action/Admin</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,7 +39,12 @@ export class BooksIndex extends Component {
                             <td>{book.id}</td>
                             <td>{book.title}</td>
                             <td>{book.description}</td>
-                            <td>{book.created}</td>
+                            <td>{book.created ? new Date(book.created).toLocaleDateString() : "-"}</td>
+                            <td className='text-nowrap'>
+                                <button onClick={() => this.editBy(book.id)} className='btn btn-sm btn-primary'>Edit</button>
+                                &nbsp;
+                                <button onClick={() => this.deleteBy(book.id)} className='btn btn-sm btn-danger'>Delete</button>
+                            </td>
                         </tr>
                     )}
                 </tbody>
@@ -42,12 +52,24 @@ export class BooksIndex extends Component {
         );
     }
 
+    goCreatePage() {
+        console.log("create");
+    }
+
+    editBy(id) {
+        console.log("Edit By " + id);
+    }
+
+    deleteBy(id) {
+        console.log("Delete By " + id);
+    }
+
     render() {
-        let contents = this.state.loading ? <p><em>Loading...</em></p> : BooksIndex.renderBooksTable(this.state.books);
+        let contents = this.state.loading ? <p><em>Loading...</em></p> : this.renderBooksTable(this.state.books);
 
         return (
             <>
-                <h1>My Books</h1>
+                <h1>My Books <button onClick={this.goCreatePage} className='btn btn-primary'><span className='fa fa-plus'>+</span></button></h1>
                 <h2>제가 집필한 책입니다.</h2>
                 {contents}
             </>
